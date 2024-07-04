abbdb1ca6676723fa0c3f98678b7fe610dcadd98
druryyl <drury_yl@yahoo.com>
2024-07-04 16:01:32 +0700

    Update Program.cs
    asdfasdf

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace Latih10_KonekDatabase
         [STAThread]
         static void Main()
         {
+            //    123123
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
